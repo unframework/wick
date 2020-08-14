fa04c26a280678f435b45486ba1e0b9a71fca651
Nick Matantsev <nick.matantsev@gmail.com>
2020-08-13 20:19:08 -0400

    Basic TypeScript build setup

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/', // ensure absolute path is used in resource references
+    filename: 'index.[contenthash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                noEmit: false
+              }
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      { test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+      { test: /\.(jpg|jpeg|gif|png|svg|eot|ttf|woff2|woff|truetype)$/, use: 'file-loader', exclude: /node_modules/ }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/index.html'
+    })
+  ],
+
+  devServer: {
+    inline: true,
+
+    proxy: {
+      // '/api': {
+      //     secure: false, // ignore certificate
+      //     target: 'https://example.com' // lab environment
+      // }
+    }
+  }
+};
