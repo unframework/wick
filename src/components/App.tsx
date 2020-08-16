9c34f313efd27d42029124e3f0f6200419a27c78
Nick Matantsev <nick.matantsev@gmail.com>
2020-08-16 00:28:51 -0400

    Show three wheels side by side

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,18 @@
 import { h, cloneElement, FunctionalComponent, VNode } from 'preact';
 import { useState } from 'preact/hooks';
 import { CRTWarpGenerator } from './CRTWarpGenerator';
-import { WheelStrip, WHEEL_STRIP_DEFS, WHEEL_HEIGHT } from './WheelStrip';
+import {
+  WheelStrip,
+  WHEEL_STRIP_DEFS,
+  WHEEL_WIDTH,
+  WHEEL_HEIGHT
+} from './WheelStrip';
 
 import './App.scss';
+
+const WHEEL_GUTTER = 10;
+const WHEEL_START_X = (320 - WHEEL_WIDTH * 3 - WHEEL_GUTTER * 2) / 2;
+const WHEEL_START_Y = 40;
 
 export const App: FunctionalComponent = () => {
   const [crtFilterNode, setCRTFilterNode] = useState<VNode | null>(null);
@@ -28,7 +37,23 @@ export const App: FunctionalComponent = () => {
           >
             <rect width="320" height="240" fill="#493c2b" />
 
-            <g transform={`translate(40,${(240 - WHEEL_HEIGHT) / 2})`}>
+            <g transform={`translate(${WHEEL_START_X},${WHEEL_START_Y})`}>
+              <WheelStrip />
+            </g>
+
+            <g
+              transform={`translate(${
+                WHEEL_START_X + WHEEL_WIDTH + WHEEL_GUTTER
+              },${WHEEL_START_Y})`}
+            >
+              <WheelStrip />
+            </g>
+
+            <g
+              transform={`translate(${
+                WHEEL_START_X + (WHEEL_WIDTH + WHEEL_GUTTER) * 2
+              },${WHEEL_START_Y})`}
+            >
               <WheelStrip />
             </g>
           </g>
