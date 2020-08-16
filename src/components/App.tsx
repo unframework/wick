2308c9c279f29634685688f96263de5906e99803
Nick Matantsev <nick.matantsev@gmail.com>
2020-08-16 00:22:33 -0400

    Implement wheel display logic basics

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,9 @@
 import { h, cloneElement, FunctionalComponent, VNode } from 'preact';
 import { useState } from 'preact/hooks';
 import { CRTWarpGenerator } from './CRTWarpGenerator';
+import { WheelStrip, WHEEL_STRIP_DEFS, WHEEL_HEIGHT } from './WheelStrip';
 
 import './App.scss';
-
-import slotIconUrl from './assets/slot-icons.png';
 
 export const App: FunctionalComponent = () => {
   const [crtFilterNode, setCRTFilterNode] = useState<VNode | null>(null);
@@ -20,43 +19,18 @@ export const App: FunctionalComponent = () => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <defs>
-          <pattern
-            id="checkerboardPattern"
-            x="0"
-            y="0"
-            width="20"
-            height="20"
-            patternUnits="userSpaceOnUse"
-            viewBox="0 0 20 20"
-          >
-            <rect fill="#f7e26b" x="0" width="10" height="10" y="0"></rect>
-            <rect fill="#f7e26b" x="10" width="10" height="10" y="10"></rect>
-          </pattern>
+          {WHEEL_STRIP_DEFS}
 
           <g
             id="screenContents"
             style="filter: url(#displacementFilter)"
             className="App__screen"
           >
-            <rect width="320" height="240" fill="#eb8931" />
-            <rect width="320" height="240" fill="url(#checkerboardPattern)" />
+            <rect width="320" height="240" fill="#493c2b" />
 
-            <image
-              xlinkHref={slotIconUrl}
-              width="960"
-              height="80"
-              x="10"
-              y="160"
-            />
-
-            <text
-              x="15"
-              y="50"
-              transform="scale(0.5, 1)"
-              transform-origin="15 120"
-            >
-              PC LOAD LETTER
-            </text>
+            <g transform={`translate(40,${(240 - WHEEL_HEIGHT) / 2})`}>
+              <WheelStrip />
+            </g>
           </g>
 
           {crtFilterNode &&
---
+++ src/components/WheelStrip.tsx
@@ -0,0 +1,79 @@
+import { h, Fragment, FunctionalComponent } from 'preact';
+
+import slotIconUrl from './assets/slot-icons.png';
+
+import './WheelStrip.scss';
+
+const ICON_SIZE = 40;
+const ICON_ATLAS_COUNT = 12;
+
+const WHEEL_STOPS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const WHEEL_STOP_COUNT = WHEEL_STOPS.length;
+
+export const WHEEL_WIDTH = ICON_SIZE * 1.5;
+export const WHEEL_HEIGHT = ICON_SIZE * 3;
+
+export const WHEEL_STRIP_DEFS = (
+  <Fragment>
+    <clipPath id="WheelStrip__overallClip">
+      <rect width={WHEEL_WIDTH} height={WHEEL_HEIGHT} />
+    </clipPath>
+    <clipPath id="WheelStrip__iconClip">
+      <rect width={ICON_SIZE} height={ICON_SIZE} />
+    </clipPath>
+  </Fragment>
+);
+
+function generateStopImage(
+  stopIncrement: number,
+  topStopIndex: number,
+  shiftOffset: number
+) {
+  const stopIndex =
+    (topStopIndex + WHEEL_STOP_COUNT - stopIncrement) % WHEEL_STOP_COUNT;
+  const iconIndex = WHEEL_STOPS[stopIndex];
+
+  return (
+    <g
+      className="_iconRoot"
+      transform={`translate(${(WHEEL_WIDTH - ICON_SIZE) / 2},${
+        (stopIncrement - shiftOffset) * ICON_SIZE
+      })`}
+    >
+      <image
+        className="_iconAtlas"
+        xlinkHref={slotIconUrl}
+        x={-iconIndex * ICON_SIZE}
+        width={ICON_SIZE * ICON_ATLAS_COUNT}
+        height={ICON_SIZE}
+      />
+    </g>
+  );
+}
+
+const WheelStripContents: FunctionalComponent = () => {
+  const wheelPosition = 0.4; // angle from centerline increasing "downwards", or clockwise if looking from left
+  const visibleMidStopPos = WHEEL_STOP_COUNT * wheelPosition;
+  const visibleTopEdgePos = visibleMidStopPos - 1.5;
+  const visibleTopStopIndex = Math.ceil(visibleTopEdgePos);
+
+  const visibleStopOffset = visibleTopStopIndex - visibleTopEdgePos;
+
+  return (
+    <Fragment>
+      {generateStopImage(0, visibleTopStopIndex, visibleStopOffset)}
+      {generateStopImage(1, visibleTopStopIndex, visibleStopOffset)}
+      {generateStopImage(2, visibleTopStopIndex, visibleStopOffset)}
+      {generateStopImage(3, visibleTopStopIndex, visibleStopOffset)}
+    </Fragment>
+  );
+};
+
+export const WheelStrip: FunctionalComponent = () => {
+  return (
+    <g className="WheelStrip">
+      <rect className="_bg" width={WHEEL_WIDTH} height={WHEEL_HEIGHT} />
+      <WheelStripContents />
+    </g>
+  );
+};
